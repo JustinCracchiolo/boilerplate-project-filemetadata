287c24dde65fd06818c52ddf963664f4d3d9b036
justincracchiolo <justincracchiolo@gmail.com>
2026-09-05 03:29:49 -0400

    able to post a file metadata

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ require('dotenv').config()
 var app = express();
 
 app.use(cors());
+
+const upload = multer()
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// POST /api/fileanalyse
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: "No file uploaded" });
+  }
 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
